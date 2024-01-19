b2ba20f7363f08f83658237b104e97da15b79c6f
mrkrimers <mrkrimerssasha@gmail.com>
2024-01-19 23:25:54 +0300

    start use swagger

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('../swagger.json');
 const cookieParser = require('cookie-parser');
 const user = require('./controller/user.controller');
 const app = express();
@@ -11,6 +13,9 @@ app.use(cors({
 }));
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
 app.use(`/user`, user);
 
 app.use((err, req, res, _next) => res.sent(err.message))
